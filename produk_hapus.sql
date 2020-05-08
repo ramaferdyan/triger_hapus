04822039cf75cd2fb05d661e11dd11dedd92d46c
ramaferdyan <62643040+ramaferdyan@users.noreply.github.com>
2020-05-08 19:29:38 +0700

    Add files via upload

---
+++ produk_hapus.sql
@@ -0,0 +1,20 @@
+DELIMITER $$
+
+USE `online_shop1`$$
+
+DROP TRIGGER /*!50032 IF EXISTS */ `hapus_barang`$$
+
+CREATE
+    /*!50017 DEFINER = 'root'@'localhost' */
+    TRIGGER `hapus_barang` AFTER DELETE ON `tb_barang` 
+    FOR EACH ROW 
+    BEGIN
+    INSERT INTO produk_hapus
+    (id_brg, nama_brg, keterangan, kategori, harga, stok, gambar, tgl_hapus, username)
+    VALUES
+    (OLD.id_brg, OLD.nama_brg, OLD.keterangan, OLD.kategori, OLD.harga, OLD.stok, OLD.gambar, SYSDATE(), CURRENT_USER);
+
+    END;
+$$
+
+DELIMITER ;
